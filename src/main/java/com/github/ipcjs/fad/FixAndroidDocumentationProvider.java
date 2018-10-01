17aee35ee942d1b08e1ee6c4cb1d9d2b09ba9eeb
JiangSong <gipcjs@gmail.com>
2018-10-01 19:48:27 +0800

    completed!!

--- src/main/java/com/github/ipcjs/fad/FixAndroidDocumentationProvider.java
+++ src/main/java/com/github/ipcjs/fad/FixAndroidDocumentationProvider.java
@@ -1,15 +1,18 @@
 package com.github.ipcjs.fad;
 
+import com.android.annotations.VisibleForTesting;
+import com.intellij.codeInsight.javadoc.JavaDocExternalFilter;
 import com.intellij.lang.documentation.AbstractDocumentationProvider;
-import com.intellij.lang.documentation.DocumentationProvider;
 import com.intellij.lang.documentation.ExternalDocumentationProvider;
+import com.intellij.lang.java.JavaDocumentationProvider;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiManager;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @see org.jetbrains.android.AndroidDocumentationProvider#fetchExternalDocumentation(Project, PsiElement, List)
@@ -17,10 +20,11 @@ import java.util.List;
 public class FixAndroidDocumentationProvider extends AbstractDocumentationProvider implements ExternalDocumentationProvider {
     private static final Logger LOG = Logger.getInstance(FixAndroidDocumentationProvider.class);
 
-    @Nullable
+
     @Override
-    public String fetchExternalDocumentation(Project project, PsiElement element, List<String> docUrls) {
-        return null;
+    public String fetchExternalDocumentation(final Project project, final PsiElement element, final List<String> docUrls) {
+        LOG.debug("fetchExternalDocumentation", docUrls);
+        return JavaDocumentationProvider.fetchExternalJavadoc(element, docUrls, new MyDocExternalFilter(project));
     }
 
     @Override
@@ -36,5 +40,38 @@ public class FixAndroidDocumentationProvider extends AbstractDocumentationProvid
     @Override
     public void promptToConfigureDocumentation(PsiElement element) {
 
+    }
+
+    @VisibleForTesting
+    static class MyDocExternalFilter extends JavaDocExternalFilter {
+        MyDocExternalFilter(Project project) {
+            super(project);
+        }
+
+        private static final Pattern ourClassDataStartPattern = Pattern.compile("START OF CLASS DATA", Pattern.CASE_INSENSITIVE);
+        private static final Pattern ourClassDataEndPattern = Pattern.compile("SUMMARY ========", Pattern.CASE_INSENSITIVE);
+//        private static final Pattern ourNonClassDataEndPattern = Pattern.compile("<A (NAME|ID)=", Pattern.CASE_INSENSITIVE);
+        private static final Pattern ourNonClassDataEndPattern = Pattern.compile("<h3 class=\"api-name\" id=\"", Pattern.CASE_INSENSITIVE);
+
+        @NotNull
+        @Override
+        protected ParseSettings getParseSettings(@NotNull String url) {
+            return url.endsWith(JavaDocumentationProvider.PACKAGE_SUMMARY_FILE) ? super.getParseSettings(url) : getParseSettingsFix(url);
+        }
+
+        @NotNull
+        protected ParseSettings getParseSettingsFix(@NotNull String url) {
+            Pattern startSection = ourClassDataStartPattern;
+            Pattern endSection = ourClassDataEndPattern;
+            boolean anchorPresent = false;
+
+            Matcher anchorMatcher = ourAnchorSuffix.matcher(url);
+            if (anchorMatcher.find()) {
+                anchorPresent = true;
+                startSection = Pattern.compile("<h3 class=\"api-name\" id=\"" + Pattern.quote(anchorMatcher.group(1)) + "\"", Pattern.CASE_INSENSITIVE);
+                endSection = ourNonClassDataEndPattern;
+            }
+            return new ParseSettings(startSection, endSection, !anchorPresent, anchorPresent);
+        }
     }
 }
